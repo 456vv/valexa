546945230ace043f63c301523ae43fd164a481dc
456vv <123@456vv.com>
2021-10-10 08:12:48 +0800

    no message

--- EchoApplication.go
+++ EchoApplication.go
@@ -6,7 +6,6 @@ import (
 
 //Echo程序的配置
 type EchoApplication struct {
-	Version					[]string									// 支持的版本号
     IsDevelop               bool                                      	// 调试
 	ValidReqTimestamp		int											// 有效时间，秒为单位
 	CertFolder				string										// 证书保存目录
